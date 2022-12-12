4544ff258f50959344d3546220d270bba04964a2
Jeffrey <aligula.jeff@gmail.com>
2022-12-12 12:30:20 +0300

    added sample data for local development testing

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
+
 const app = express();
 
 // Allow cross-origin requests
 app.use(cors());
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}));
 
 
 app.listen(port, () => {
